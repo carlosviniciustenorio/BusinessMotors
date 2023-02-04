b5dd158e32c9d63f4cf726b07fba1aab3dcb24df
Carlos Vinícius <47374132+carlosviniciustenorio@users.noreply.github.com>
2023-02-04 09:17:38 -0300

    adjusting app

--- src/CManager/CManager.API/Program.cs
+++ src/CManager/CManager.API/Program.cs
@@ -1,21 +1,11 @@
-using CManager.Infrastructure.HostedServices;
 using Microsoft.Net.Http.Headers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-// builder.Services.AddHttpClient<GitHubService>(httpClient =>
-// {
-//     httpClient.BaseAddress = new Uri("https://api.github.com/");
-//     httpClient.DefaultRequestHeaders.Add(HeaderNames.Accept, "pplication/vnd.github.v3+json");
-//     httpClient.DefaultRequestHeaders.Add(HeaderNames.UserAgent, "HttpRequestsSample");
-//     httpClient.Timeout = TimeSpan.FromSeconds(15);
-// });
 
-// builder.Services.AddHostedService<GitHubService>();
 builder.Services.AddHealthChecks();
 
 var app = builder.Build();
@@ -32,6 +22,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseRouting();
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapHealthChecks("/health");
