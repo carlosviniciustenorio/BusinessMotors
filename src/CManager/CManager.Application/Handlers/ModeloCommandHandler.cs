acfe42bab30fdab0d9b08ab78450dda841e91fb9
Carlos Vinícius <47374132+carlosviniciustenorio@users.noreply.github.com>
2023-03-06 22:16:37 -0300

    commands, handlers e controllers to version and model

--- src/CManager/CManager.Application/Handlers/ModeloCommandHandler.cs
+++ src/CManager/CManager.Application/Handlers/ModeloCommandHandler.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace CManager.Application.Handlers
 {
     public class ModeloCommandHandler : IRequestHandler<AddModeloCommand.ModeloCommand, Unit>
