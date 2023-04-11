34b07da18703c81930feb48a37589e95850ebeaf
Carlos Vinícius <47374132+carlosviniciustenorio@users.noreply.github.com>
2023-04-11 19:09:05 -0300

    repository

--- src/CManager/CManager.Infrastructure/Repositories/AnuncioRepository.cs
+++ src/CManager/CManager.Infrastructure/Repositories/AnuncioRepository.cs
@@ -1,12 +1,11 @@
 namespace CManager.Infrastructure.Repositories
 {
-    public class AnuncioRepository: IAnuncioRepository
+    public class AnuncioRepository: Repository<Anuncio>, IAnuncioRepository
     {
         protected DbSet<Anuncio> _dbSet;
         protected CManagerDBContext _dbContext;
 
-        public AnuncioRepository(CManagerDBContext dbContext) => (_dbSet, _dbContext) = (dbContext.Set<Anuncio>(), dbContext);
-        public async Task AddAsync(Anuncio caracteristica) => await _dbSet.AddAsync(caracteristica);        
+        public AnuncioRepository(CManagerDBContext dbContext) : base(dbContext) => (_dbSet, _dbContext) = (dbContext.Set<Anuncio>(), dbContext);
         public async Task<List<Anuncio>> GetListByIdAsync(List<Guid> ids) => await _dbSet.Where(d => ids.Contains(d.Id)).ToListAsync();
         public async Task<Anuncio> GetByIdAsync(Guid id) => await _dbSet.IgnoreAutoIncludes()
                                                                         .Include(d => d.Caracteristicas)
@@ -21,6 +20,5 @@ namespace CManager.Infrastructure.Repositories
                                                                         .ThenInclude(d => d.Marca)
                                                                       .Include(d => d.Versao)
                                                                       .ToListAsync();
-        public async Task SaveChangesAsync() => await _dbContext.SaveChangesAsync();
     }
 }
