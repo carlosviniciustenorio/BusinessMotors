63e0956e05d7db74b4f21809536282bac5d1a6a8
Carlos Vinícius <47374132+carlosviniciustenorio@users.noreply.github.com>
2023-05-18 18:33:31 -0300

    allow secrets manager

--- infra-2/ecs_module/main.tf
+++ infra-2/ecs_module/main.tf
@@ -26,12 +26,9 @@ resource "aws_iam_policy" "policies" {
     Version   = "2012-10-17"
     Statement = [
       {
-        Sid     = "AllowSecretsManagerAccess"
+        Sid     = "Stmt1684445489794"
+        Action  = "secretsmanager:*",
         Effect  = "Allow"
-        Action  = [
-          "secretsmanager:GetSecretValue",
-          "secretsmanager:DescribeSecret"
-        ]
         Resource = "*"
       },
       {
