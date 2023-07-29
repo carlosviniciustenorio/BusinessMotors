1d2bba4d4293b79dd72d2cea4486148cb420eb04
Carlos Vinícius <47374132+carlosviniciustenorio@users.noreply.github.com>
2023-07-29 11:00:56 -0300

    adicionando documentação com swagger

--- src/CManager/CManager.API/Program.cs
+++ src/CManager/CManager.API/Program.cs
@@ -1,3 +1,6 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var env = builder.Environment;
 var config = builder.Configuration;
@@ -49,8 +52,56 @@ builder.Services.AddControllers()
                 .AddFluentValidation(options => { options.RegisterValidatorsFromAssemblyContaining<GetAnunciosQuery.Anuncios>();});
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "CManager.Api",
+        Version = "v1"
+    });
+
+    options.SwaggerDoc("v2", new OpenApiInfo
+    {
+        Title = "CManager.Api",
+        Version = "v2"
+    });
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. 
+                        Enter 'Bearer' [space] and then your token in the text input below. 
+                        Example: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+            },
+            Scheme = "oauth2",
+            Name = "Bearer",
+            In = ParameterLocation.Header,
+
+            },
+            new List<string>()
+        }
+    });
+});
+
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
-builder.Services.AddSwaggerGen();
 builder.Services.AddAuthentication(builder.Configuration, jwtOptions);
 builder.Services.AddHealthChecks();
 builder.Services.AddTransient<ExceptionLoggingMiddleware>();
---
+++ src/CManager/CManager.Tests/Infrastructure/AnuncioRepositoryTest.cs
@@ -0,0 +1,24 @@
+﻿using CManager.Infrastructure.Context.CManager;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CManager.Tests.Infrastructure
+{
+    public class AnuncioRepositoryTest
+    {
+        [Fact]
+        public void Test()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<CManagerDBContext>().UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var context = new CManagerDBContext(optionsBuilder.Options);
+
+            context.Opcionais.Add(new ("teste"));
+            context.SaveChanges();
+            Assert.Single(context.Opcionais);
+        }
+    }
+}
